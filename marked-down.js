57b7329edb6af63732541b092b0eea8e41507563
Travis Reeder <treeder@gmail.com>
2025-05-28 00:22:43 +0000

    Fix spacing

--- marked-down.js
+++ marked-down.js
@@ -28,76 +28,79 @@ export class MarkedDown extends LitElement {
 
   static styles = [
     css`
-        p {
-              margin-block-start: 0;
-        }
-        a {
-            color: var(--md-sys-color-primary); /* https://developer.mozilla.org/en-US/docs/Web/CSS/var#syntax */
-        }
-        a:hover {
-            text-decoration: underline;
-        }
-        h1, h2, h3, h4, h5, h6, p {
-          margin-bottom: 20px;
-        }
-        ul, ol {
-          margin-top: 4px;
-          margin-bottom: 4px;
-        }
-        li {
-          padding-top: 4px;
-          padding-bottom: 4px;
-        }
+    :host {
+      display: block;
+    }
+    p {
+      margin-block-start: 0;
+    }
+    a {
+      color: var(--md-sys-color-primary); /* https://developer.mozilla.org/en-US/docs/Web/CSS/var#syntax */
+    }
+    a:hover {
+      text-decoration: underline;
+    }
+    h1, h2, h3, h4, h5, h6, p {
+      margin: .67em 0;
+    }
+    ul, ol {
+      margin-top: 4px;
+      margin-bottom: 4px;
+    }
+    li {
+      padding-top: 4px;
+      padding-bottom: 4px;
+    }
 
-        img {
-            max-width: 100%;
-            border: 1px solid silver;
-            padding: 8px;
-            border-radius: 8px;
-            margin: 0 auto;
-        }
+    img {
+      max-width: 100%;
+      border: 1px solid silver;
+      padding: 8px;
+      border-radius: 8px;
+      margin: 0 auto;
+    }
 
-        code:not(pre) {
-            background-color: var(--marked-down-inline-code-background-color, #22272e);
-            color: var(--marked-down-inline-code-color, #adbac7);
-            padding: 0.2em 0.4em;
-            border-radius: 6px;
-        }
+    code:not(pre) {
+      background-color: var(--marked-down-inline-code-background-color, #22272e);
+      color: var(--marked-down-inline-code-color, #adbac7);
+      padding: 0.2em 0.4em;
+      border-radius: 6px;
+    }
 
-        /** pre is for triple tick blocks */
-        pre {
-            /* display:block; */
-            overflow-x: auto;
-            padding: 16px;
-            border: 1px solid var(--md-sys-color-outline);
-            border-radius: 6px;
-            color:#adbac7;
-            background:#22272e;
-        }
+    /** pre is for triple tick blocks */
+    pre {
+        /* display:block; */
+        overflow-x: auto;
+        padding: 16px;
+        border: 1px solid var(--md-sys-color-outline);
+        border-radius: 6px;
+        color:#adbac7;
+        background:#22272e;
+    }
 
-        pre code {
-            padding: 0;
-        }
+    pre code {
+        padding: 0;
+    }
 
-          /* This is the CSS directly from highlight.js
-            list of themes here: https://highlightjs.org/examples
-            see https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.7.0/build/styles/
-          */
-        /*!
-        Theme: GitHub Dark Dimmed
-        Description: Dark dimmed theme as seen on github.com
-        Author: github.com
-        Maintainer: @Hirse
-        Updated: 2021-05-15
+      /* This is the CSS directly from highlight.js
+        list of themes here: https://highlightjs.org/examples
+        see https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.7.0/build/styles/
+      */
+    /*!
+    Theme: GitHub Dark Dimmed
+    Description: Dark dimmed theme as seen on github.com
+    Author: github.com
+    Maintainer: @Hirse
+    Updated: 2021-05-15
 
-        Colors taken from GitHub's CSS
-        */
-        .hljs{
-            color:#adbac7;
-            background:#22272e;
-        }
-        .hljs-doctag,.hljs-keyword,.hljs-meta .hljs-keyword,.hljs-template-tag,.hljs-template-variable,.hljs-type,.hljs-variable.language_{color:#f47067}.hljs-title,.hljs-title.class_,.hljs-title.class_.inherited__,.hljs-title.function_{color:#dcbdfb}.hljs-attr,.hljs-attribute,.hljs-literal,.hljs-meta,.hljs-number,.hljs-operator,.hljs-selector-attr,.hljs-selector-class,.hljs-selector-id,.hljs-variable{color:#6cb6ff}.hljs-meta .hljs-string,.hljs-regexp,.hljs-string{color:#96d0ff}.hljs-built_in,.hljs-symbol{color:#f69d50}.hljs-code,.hljs-comment,.hljs-formula{color:#768390}.hljs-name,.hljs-quote,.hljs-selector-pseudo,.hljs-selector-tag{color:#8ddb8c}.hljs-subst{color:#adbac7}.hljs-section{color:#316dca;font-weight:700}.hljs-bullet{color:#eac55f}.hljs-emphasis{color:#adbac7;font-style:italic}.hljs-strong{color:#adbac7;font-weight:700}.hljs-addition{color:#b4f1b4;background-color:#1b4721}.hljs-deletion{color:#ffd8d3;background-color:#78191b}
-        `]
+    Colors taken from GitHub's CSS
+    */
+    .hljs{
+        color:#adbac7;
+        background:#22272e;
+    }
+    .hljs-doctag,.hljs-keyword,.hljs-meta .hljs-keyword,.hljs-template-tag,.hljs-template-variable,.hljs-type,.hljs-variable.language_{color:#f47067}.hljs-title,.hljs-title.class_,.hljs-title.class_.inherited__,.hljs-title.function_{color:#dcbdfb}.hljs-attr,.hljs-attribute,.hljs-literal,.hljs-meta,.hljs-number,.hljs-operator,.hljs-selector-attr,.hljs-selector-class,.hljs-selector-id,.hljs-variable{color:#6cb6ff}.hljs-meta .hljs-string,.hljs-regexp,.hljs-string{color:#96d0ff}.hljs-built_in,.hljs-symbol{color:#f69d50}.hljs-code,.hljs-comment,.hljs-formula{color:#768390}.hljs-name,.hljs-quote,.hljs-selector-pseudo,.hljs-selector-tag{color:#8ddb8c}.hljs-subst{color:#adbac7}.hljs-section{color:#316dca;font-weight:700}.hljs-bullet{color:#eac55f}.hljs-emphasis{color:#adbac7;font-style:italic}.hljs-strong{color:#adbac7;font-weight:700}.hljs-addition{color:#b4f1b4;background-color:#1b4721}.hljs-deletion{color:#ffd8d3;background-color:#78191b}
+    `]
 
   static properties = {
     content: { type: String }, // value for QR code
